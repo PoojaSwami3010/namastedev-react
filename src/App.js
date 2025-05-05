15913430d501c77b3cf0bf702503f42cc5462b76
PoojaSwami3010 <2017mit014@sggs.ac.in>
2025-05-05 11:08:56 +0530

    new app

--- src/App.js
+++ src/App.js
@@ -2,36 +2,67 @@ import React from 'react';
 import './style.css';
 // import './HeadingComponent';
 
+
+/* 
+Header 
+  -Logo
+  - Nav items
+Body
+  -Search
+  -Restaurantcontaniner
+    -restaurant cards
+
+Footer
+  -copyright
+  -links  
+  -address
+  -contact
+
+
+
+*/
+
+
+const Header=()=>{
+  return (
+    <div className='header'>
+      <div className='logo-container'>
+      <img className='logo' src="https://plus.unsplash.com/premium_vector-1713201017274-e9e97d783e75?q=80&w=2128&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
+      </div>
+      <div className='nav-items'>
+        <ul>
+          <li>Home
+          </li>
+          <li>
+            About Us
+            </li>
+            <li>
+            Contact US
+            </li>
+            <li>
+            Cart
+            </li>
+        </ul>
+
+      </div>
+      
+
+    </div>
+  )
+}
+
+
+
+
+
+
+
 export default function App() {
   return (
-    <div>
-      {/* <h1 className="test">Hello StackBlitz!</h1> */}
-      <Heading />
-      {/* or */}
-      or
-      {Heading()}
-      or 
-      <Heading></Heading>
-       {/*<Title /> */}
-    <h2>{
-title
-      }</h2>  
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <div className="app">
+      <Header></Header>
+      </div>
   );
 }
 
-const number=1000;
-// react element
-const heading = <h1 className="test"></h1>;
-
-// react component Heading
-const Heading = () => {
-  return <h1>Heading Functional Component</h1>;
-};
-
-const elem=<span>React Element</span>
-const title=(
-  <h1>this is title element  & {elem}</h1>
-);
 
