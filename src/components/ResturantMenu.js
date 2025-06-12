cb35c00aab1062b78b078e89712ebc62d48c7216
PoojaSwami3010 <2017mit014@sggs.ac.in>
2025-06-12 18:11:08 +0530

    new changes

--- src/components/ResturantMenu.js
+++ src/components/ResturantMenu.js
@@ -10,23 +10,25 @@ useEffect(()=>{
 featchMenu();
 },[])
 
+
+// console.log("res menu data",resData)
 const featchMenu=async ()=>{
 
     // const data=await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5204303&lng=73.8567437&restaurantId=11887&catalog_qa=undefined&submitAction=ENTER")
+// console.log("res menu data",resData,resData?.data?.cards[2].card.card.info)
+    // const json1=resData;
 
-    const json=resData;
-
-    console.log(json)
-
+    // console.log(json1)
+setResInfo(resData?.data?.cards[2].card.card.info)
 }
-const {name,cuisines,costForTowMessage}=json.cards[0].card.card.info
+const {name,cuisines,costForTwoMessage,id}=resData?.data?.cards[2].card.card.info
 
 
     return resInfo===null ? <Shimmer />: (
         <div className="menu">
             <h1>{name}</h1>
             <h2>{cuisines}</h2>
-            <h2>{costForTowMessage}</h2>
+            <h2>{costForTwoMessage}</h2>
            
         </div>
     )
