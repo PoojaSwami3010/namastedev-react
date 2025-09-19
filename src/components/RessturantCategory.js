48606599d7acd836080ae107a2b2bf2d109a6545
null <null>
2025-09-19 16:28:28 +0530

    accordian changes

--- src/components/RessturantCategory.js
+++ src/components/RessturantCategory.js
@@ -1,20 +1,27 @@
+import { useState } from "react";
 import ItemList from "./ItemList";
 
 const RessturantCategory=(data)=>{
     // console.log("data in categry",data)
+    const [showItem,setShowItems]=useState(false)
+
+    const handleClick=()=>{
+        console.log("clicked")
+        setShowItems(!showItem)
+    }
+
+
     return <div>
     {/* accrdian header section */}
     
     <div className="w-6/12 m-auto bg-gray-50 my-4 p-4 shadow-lg">
-        <div className="flex justify-between">
-
-       
+        <div className="flex justify-between cursor-pointer" onClick={handleClick}>       
 <span className="font-bold text-lg">{data.data.name}
 
     </span>
     <span className="text-red">v</span>
     </div>
-    <ItemList  items={data.data}/>
+   { showItem ? <ItemList  items={data.data}/> : ''}
     </div>
     {/* accordian body */}
 
