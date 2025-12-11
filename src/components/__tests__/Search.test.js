d18d1728e048d6f87f5e86ef51a0c36133a1d1ad
Pooja Swami <63848720+PoojaSwami3010@users.noreply.github.com>
2025-12-11 10:37:35 +0000

    changes

--- src/components/__tests__/Search.test.js
+++ src/components/__tests__/Search.test.js
@@ -1,11 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import Body from "../Body";
+import resData from "../../utils/mockDataforSingleCard";
 
 import "@testing-library/jest-dom"
 
 global.fetch=jest.fn(()=>{
     return Promise.resolve({
-        json:()=>{return Promise.resolve(data)}
+        json:()=>{return Promise.resolve(resData)}
     });
 });
 
